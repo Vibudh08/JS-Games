41b82b3bfbb576d00329a4d75bcb1a00a2208ad5
Vibudh08 <vibudhrathore8@gmail.com>
2023-11-09 23:57:16 +0530

    mole pic

---
+++ Whack-A-Mole/script.js
@@ -0,0 +1,49 @@
+const square = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const timeLeft = document.querySelector('#time-left')
+const score = document.querySelector('#score')
+
+let result = 0
+let hitPosition
+let timerId
+let currentTime = 10
+
+function randomSquare(){
+    square.forEach(square =>{
+        square.classList.remove('mole')
+    })
+
+    let randomSquare = square[Math.floor(Math.random()*9)]
+    randomSquare.classList.add('mole')
+
+    hitPosition = randomSquare.id
+}
+randomSquare()
+
+square.forEach(square =>{
+    square.addEventListener('mousedown',()=>{
+        if(square.id == hitPosition){
+            result++
+            score.textContent = result
+            hitPosition = null
+        }
+    })
+})
+
+function moveMole(){
+    timerId = setInterval(randomSquare,1000)
+}
+moveMole()
+
+function countDown(){
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if(currentTime == 0){
+        clearInterval(countDownTimerId)
+        clearTimeout(timerId)
+        alert('Game Over, Your final score is ' + result)
+    }
+}
+
+let countDownTimerId = setInterval(countDown, 1000)
